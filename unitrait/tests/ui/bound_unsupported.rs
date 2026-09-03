d06568fc52fcd5bb481cd90fde961776ab94f103
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2026-09-03 15:44:44 +0200

    Fix unsupported bound test.

--- unitrait/tests/ui/bound_unsupported.rs
+++ unitrait/tests/ui/bound_unsupported.rs
@@ -1,8 +1,7 @@
 unitrait::unitrait! {
     pub trait Foo {
         #[opaque(size = 8, align = 4)]
-        #[drop_symbol = "_ui_bound_unsupported_drop"]
-        pub type Context: Clone + Drop;
+        pub type Context: Default;
 
         #[symbol = "_ui_bound_unsupported_new"]
         pub fn new() -> Self::Context;
